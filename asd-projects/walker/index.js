5f85533c0fea484a025bd4c88c0a9054c5f356d9
PChea2019 <PhilipChea18@gmail.com>
2026-06-16 15:58:44 +0000

    Finished walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -18,10 +18,21 @@ function runProgram(){
     UP: 38,
     RIGHT: 39,
     DOWN: 40,
+    SPACE: 32,
+    W: 87,
+    A: 65,
+    S: 83,
+    D: 68,
   };
   var walker = {
     x: 0,
     y: 0,
+    speedX: 0,
+    speedY: 0,
+  }
+    var walkerZ = {
+    x: 50,
+    y: 50,
     speedX: 0,
     speedY: 0,
   }
@@ -34,7 +45,8 @@ function runProgram(){
 
   Note: You can have multiple event listeners for different types of events.
   */
-  $(document).on('keydown', handleKeyDown);                          
+  $(document).on('keydown', handleKeyDown);     
+  $(document).on('keyup', handleKeyUp)                     
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -47,7 +59,10 @@ function runProgram(){
   function newFrame() {
     repositionGameItem()
     // console.log(walker.x, walker.y)
+    wallCollision()
+    boxCollision()
     redrawGameItem()
+    
 
   }
   
@@ -75,7 +90,69 @@ function runProgram(){
       console.log("down pressed")
       walker.speedY = 5
     }
+    if (event.which === KEY.ENTER) {
+      console.log ("enter pressed")
+      makeMine(walker.x + 20, walker.y + 20, "#mine")
+    }
+
+    if (event.which === KEY.A) {
+      console.log("A pressed")
+      walkerZ.speedX = -5
+    }
+    if (event.which === KEY.D) {
+      console.log("D pressed")
+      walkerZ.speedX = 5
+    }
+    if (event.which === KEY.W) {
+      console.log("W pressed")
+      walkerZ.speedY = -5
+    }
+    if (event.which === KEY.S) {
+      console.log("S pressed")
+      walkerZ.speedY = 5
+    }
+    if (event.which === KEY.SPACE) {
+      console.log ("Space pressed")
+      makeMineZ (walkerZ.x + 20, walkerZ.y + 20, "#mineZ")
+    }
   }
+  function handleKeyUp (event) {
+    if (event.which === KEY.LEFT) {
+      console.log("left released")
+      walker.speedX = 0
+    }
+    if (event.which === KEY.RIGHT) {
+      console.log("right released")
+      walker.speedX = 0
+    }
+    if (event.which === KEY.UP) {
+      console.log("up released")
+      walker.speedY = 0
+    }
+    if (event.which === KEY.DOWN) {
+      console.log("down released")
+      walker.speedY = 0
+    }
+
+    if (event.which === KEY.A) {
+      console.log("A released")
+      walkerZ.speedX = 0
+    }
+    if (event.which === KEY.D) {
+      console.log("D released")
+      walkerZ.speedX = 0
+    }
+    if (event.which === KEY.W) {
+      console.log("W released")
+      walkerZ.speedY = 0
+    }
+    if (event.which === KEY.S) {
+      console.log("S released")
+      walkerZ.speedY = 0
+    }
+  }
+
+  
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
@@ -83,11 +160,15 @@ function runProgram(){
   function repositionGameItem() {
     walker.x = walker.x + walker.speedX
     walker.y = walker.y + walker.speedY
+    walkerZ.x = walkerZ.x + walkerZ.speedX
+    walkerZ.y = walkerZ.y + walkerZ.speedY
 
   }
   function redrawGameItem(){
     $("#walker").css("left", walker.x)
     $("#walker").css("top", walker.y)
+    $("#walkerZ").css("left", walkerZ.x)
+    $("#walkerZ").css("top", walkerZ.y)
   }
   
   function endGame() {
@@ -97,5 +178,63 @@ function runProgram(){
     // turn off event handlers
     $(document).off();
   }
+  function wallCollision() {
+    if (walker.x < 0) {
+      walker.x -= walker.speedX
+    }
+    if (walker.y < 0) {
+      walker.y -= walker.speedY
+    }
+    if (walker.x + 50 > $("#board").width()) {
+      walker.x -= walker.speedX
+    }
+    if (walker.y + 50 > $("#board").height()) {
+      walker.y -= walker.speedY
+    }
+
+    if (walkerZ.x < 0) {
+      walkerZ.x -= walkerZ.speedX
+    }
+    if (walkerZ.y < 0) {
+      walkerZ.y -= walkerZ.speedY
+    }
+    if (walkerZ.x + 50 > $("#board").width()) {
+      walkerZ.x -= walkerZ.speedX
+    }
+    if (walkerZ.y + 50 > $("#board").height()) {
+      walkerZ.y -= walkerZ.speedY
+    }
+  }
+  function boxCollision() {
+    if (walker.x < walkerZ.x + 50 && walker.x + 50 > walkerZ.x && walker.y < walkerZ.y + 50 && walker.y + 50 > walkerZ.y){
+      walker.x -= walker.speedX
+      walker.y -= walker.speedY
+      walkerZ.x -= walkerZ.speedX
+      walkerZ.y -= walkerZ.speedY
+    }
+  }
+    function makeMine (x, y, mined){
+      $(mined).empty()
+    $("<div>")
+      .css("height", 15)
+      .css("width", 15)
+      .css("background-color", "purple")
+      .css("position", "absolute")
+      .css("left", x)
+      .css("top", y)
+      .appendTo("#mine")
   
+  }
+    function makeMineZ (x, y, mined){
+      $(mined).empty()
+    $("<div>")
+      .css("height", 15)
+      .css("width", 15)
+      .css("background-color", "Orchid")
+      .css("position", "absolute")
+      .css("left", x)
+      .css("top", y)
+      .appendTo("#mineZ")
+  
+  }
 }
